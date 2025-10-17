24f395a7ab571bb5ace070583542b75ef1cdb88c
Armaan Chhina <armaan_45@hotmail.com>
2025-10-16 19:05:45 -0700

    Create simple footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+      <footer className="text-center py-8 text-sm text-gray-500">
+        <p>Your resume is stored securely and never shared</p>
+      </footer>
+    );
+}
+  
+  
