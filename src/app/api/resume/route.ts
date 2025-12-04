8131fd4a5349d8fdfb7880a09bb9fbb00562b243
Armaan Chhina <armaan_45@hotmail.com>
2025-12-03 19:22:58 -0800

    Setup API route to fetch resume

--- src/app/api/resume/route.ts
+++ src/app/api/resume/route.ts
@@ -31,7 +31,13 @@ export async function POST(req: Request) {
 
       const body = await req.json();
 
-      const resume = await prisma.resume.upsert({
+
+      const rawSkills = body.skills?.technical || "";
+      const skills = rawSkills.split(",")
+        .map((s: string) => s.trim())
+        .filter(Boolean);
+        
+        const resume = await prisma.resume.upsert({
         where: {userId},
         update: {
           fullName: body.personalInfo.fullName,
@@ -41,7 +47,7 @@ export async function POST(req: Request) {
           github: body.personalInfo.github,
           portfolio: body.personalInfo.portfolio,
           summary: body.summary,
-          skills: body.skills.technical,  // Array directly
+          skills: skills,  // Array directly
           workJson: body.workExperience,
           educationJson: body.education,
         },
@@ -54,7 +60,7 @@ export async function POST(req: Request) {
           github: body.personalInfo.github,
           portfolio: body.personalInfo.portfolio,
           summary: body.summary,
-          skills: body.skills.technical,
+          skills: skills,
           workJson: body.workExperience,
           educationJson: body.education,
         }
@@ -68,9 +74,52 @@ export async function POST(req: Request) {
   }
 
 export async function GET() {
-  return NextResponse.json({
-    ok: true,
-    route: "/api/resume",
-    message: "Resume API route is working (dummy GET).",
-  });
+  try{
+    const store = await cookies()
+    const sessionToken = store.get("session")?.value
+
+    if (!sessionToken) {
+      return NextResponse.json(
+        {ok: false, error: "Not authenticated"},
+        {status: 401}
+      )
+    }
+
+    const session = await prisma.session.findUnique({
+      where: { id: sessionToken}
+    })
+
+    if ( !session ) {
+      return NextResponse.json(
+        { okay: false, error: "Invalid Session"},
+        {status: 401}
+      )
+    }
+
+    const userId = session.userId
+
+    const resume = await prisma.resume.findUnique({
+      where: {userId}
+    })
+
+    if (!resume) {
+      return NextResponse.json({
+        ok: true,
+        hasResume: false,
+        resume: null
+      })
+    }
+
+    return NextResponse.json({
+      ok: true,
+      hasResume: true,
+      resume
+    })
+  } catch (err) {
+    console.error("Resume GET error:", err);
+    return NextResponse.json(
+      { ok: false, error: "Server error", hasResume: false },
+      { status: 500 }
+    );
+  }
 }
